b91a8c29976f05b2171586edf771dc8dd4b2356d
NicolasLima01 <nicolasrlima@protonmail.com>
2025-01-29 16:22:53 -0300

    criado cadastro.php

---
+++ cadastro.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastro</title>
+</head>
+<body>
+<form action="submit">
+            <h2>Cadastro</h2>
+            Nome<input type=text>
+            Email<input type=text>
+            Telefone<input type=text>
+            Senha<input type=text>
+            <button>Cadastrar</button>
+        </form>
+</body>
+</html>
