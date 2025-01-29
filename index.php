e2bf2c02174c215e12e65c5d2f08c399beccf3c2
NicolasLima01 <nicolasrlima@protonmail.com>
2025-01-29 15:01:41 -0300

    Inicio do site, criando index.php e .css, usando grid no main

---
+++ index.php
@@ -0,0 +1,60 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GreenIT</title>
+    <link rel="stylesheet" href="index.css">
+</head>
+
+<body>
+
+    <header>
+        <h1>GreenIT</h1>
+        <nav>
+            <menu>
+                <li><a href="">Conceitos</a></li>
+                <li><a href="">Importância</a></li>
+                <li><a href="">Benefícios</a></li>
+                <li><a href="">Cadastro</a></li>
+            </menu>
+        </nav>
+    </header>
+
+    <main>
+        <section class="topico-sessao">
+            <h2>O que é GreenIT?</h2>
+            <p>Seu conceito compreende práticas que visam preservar o meio ambiente. Isto é, fazer uso dos recursos computacionais da maneira mais ‘limpa’ possível, prezando para que as atividades envolvidas nos avanços da TI não tragam tantas consequências negativas para o planeta.</p>
+        </section>
+
+        <section class="topico-sessao">
+            <h2>Importância da GreenIT</h2>
+            <p></p>
+        </section>
+
+        <section class="topico-sessao">
+            <h2>Benefícios</h2>
+            <menu>
+                <li>Redução de custos</li>
+                <li>Dá asas ao branding</li>
+                <li>Renova os processos</li>
+                <li>Cuida do ecossistema</li>
+            </menu>
+        </section>
+
+        <form action="submit">
+            <h2>Cadastro</h2>
+            Nome<input type=text>
+            Email<input type=text>
+            Telefone<input type=text>
+            Senha<input type=text>
+            <button>Cadastrar</button>
+        </form>
+
+    </main>
+    <?php
+    ?>
+</body>
+
+</html>
